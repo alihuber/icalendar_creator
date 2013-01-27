3c73f0c2420afdc8d1efef7640a15fddb33d4c43
Alexander Huber <alih83@gmx.de>
2013-01-27 15:59:54 +0100

    added more tests for ical-string creation

--- spec/ical_creator_spec.rb
+++ spec/ical_creator_spec.rb
@@ -45,28 +45,45 @@ describe "icalendar string creation" do
   let(:event) { Event.new({"name" => 'Test', "start_date" => '23.02.2012',
                           "start_time" => '11:00', "end_date" => '24.02.2012',
                           "end_time" => '12:00', "location" => 'Loc', "description" => 'Desc', "repetition_freq" => ""}) }
+
   let(:event2) { Event.new({"name" => 'Test2', "start_date" => '23.02.2012',
                           "start_time" => '11:00', "end_date" => '24.02.2012',
                           "end_time" => '12:00', "location" => 'Loc2', "description" => 'Desc2', "repetition_freq" => ""}) }
+
   let(:event3) { Event.new({"name" => 'Test3', "start_date" => '23.02.2012',
                           "start_time" => '11:00', "end_date" => '24.02.2012',
                           "end_time" => '12:00', "location" => 'Loc3', "description" => 'Desc3', "repetition_freq" => "Wöchentlich",
                           "repetition_interval" => ""}) }
+
+  let(:event4) { Event.new({"name" => 'Test4', "start_date" => '02/23/2012',
+                          "start_time" => '11:00', "end_date" => '02/24/2012',
+                          "end_time" => '12:00', "location" => 'Loc4', "description" => 'Desc4', "repetition_freq" => "Daily",
+                          "repetition_interval" => "2"}) }
   let(:icc) { ICalendarCreator.new }
 
-  test_string = "BEGIN:VCALENDAR\n" + "VERSION:2.0\n" + "PRODID:-//ICalendarCreator//NONSGML//EN\n" +
+  test_string1 = "BEGIN:VCALENDAR\n" + "VERSION:2.0\n" + "PRODID:-//ICalendarCreator//NONSGML//EN\n" +
     "BEGIN:VEVENT\n" + "DTSTART:20120223T110000\n" + "DTEND:20120224T120000\n" + "SUMMARY:Test\n" +
-    "LOCATION:Loc\n" + "DESCRIPTION:Desc\n" +"END:VEVENT\n" + "BEGIN:VEVENT\n" + "DTSTART:20120223T110000\n" +
-    "DTEND:20120224T120000\n"  + "SUMMARY:Test2\n" + "LOCATION:Loc2\n" + "DESCRIPTION:Desc2\n" + "END:VEVENT\n" +
+    "LOCATION:Loc\n" + "DESCRIPTION:Desc\n" +"END:VEVENT\n" +
     "BEGIN:VEVENT\n" + "DTSTART:20120223T110000\n" +
-    "DTEND:20120224T120000\n" + "RRULE:FREQ=WEEKLY;INTERVAL=1\n" + "SUMMARY:Test3\n" + "LOCATION:Loc3\n" + "DESCRIPTION:Desc3\n" + "END:VEVENT\n" +
+    "DTEND:20120224T120000\n"  + "SUMMARY:Test2\n" + "LOCATION:Loc2\n" + "DESCRIPTION:Desc2\n" + "END:VEVENT\n" +
     "END:VCALENDAR\n"
 
-  it "should be creating a correct iCalendar string" do
+  test_string2 = "BEGIN:VCALENDAR\n" + "VERSION:2.0\n" + "PRODID:-//ICalendarCreator//NONSGML//EN\n" +
+    "BEGIN:VEVENT\n" + "DTSTART:20120223T110000\n" +
+    "DTEND:20120224T120000\n" + "RRULE:FREQ=WEEKLY;INTERVAL=1\n" + "SUMMARY:Test3\n" + "LOCATION:Loc3\n" + "DESCRIPTION:Desc3\n" + "END:VEVENT\n" +
+    "BEGIN:VEVENT\n" + "DTSTART:20120223T110000\n" +
+    "DTEND:20120224T120000\n" + "RRULE:FREQ=DAILY;INTERVAL=2\n" + "SUMMARY:Test4\n" + "LOCATION:Loc4\n" + "DESCRIPTION:Desc4\n" + "END:VEVENT\n" +
+    "END:VCALENDAR\n"
+
+  it "should be creating a correct iCalendar string with no repetition" do
     icc.add_event(1, event)
     icc.add_event(2, event2)
-    icc.add_event(3, event3)
-    icc.create_icalendar.should eq test_string
+    icc.create_icalendar.should eq test_string1
   end
 
+  it "should be creating a correct iCalendar string with repetition and mixed input languages" do
+    icc.add_event(1, event3)
+    icc.add_event(2, event4)
+    icc.create_icalendar.should eq test_string2
+  end
 end
